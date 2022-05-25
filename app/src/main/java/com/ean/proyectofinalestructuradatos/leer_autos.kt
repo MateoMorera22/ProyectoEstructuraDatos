e972e8a3fa79f56f11793e8667443c4d60363922
javierzs <jzarate54035@universidadean.edu.co>
2022-05-25 00:22:09 -0500

    Actualización Vista Imagenes

--- app/src/main/java/com/ean/proyectofinalestructuradatos/leer_autos.kt
+++ app/src/main/java/com/ean/proyectofinalestructuradatos/leer_autos.kt
@@ -1,51 +1,69 @@
 package com.ean.proyectofinalestructuradatos
 
 
+import android.app.ProgressDialog
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
-import android.widget.MediaController
-import android.widget.VideoView
+import android.widget.Toast
+import com.ean.proyectofinalestructuradatos.databinding.ActivityMainBinding
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class leer_autos : AppCompatActivity() {
-    val storage = Firebase.storage
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    val storage = Firebase.storage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_leer_autos)
 
-        //Leer el video de storageRef
-        var nombre_auto=intent.getStringExtra("TITULO AUTO").toString()
-
-        var video=findViewById<VideoView>(R.id.videoView)
-        val Ref = storage.reference.child("autos")
-
-        Log.d("Firebase", "Files $nombre_auto")
-
-        Ref.child(nombre_auto).downloadUrl.addOnSuccessListener {
-            Log.d("item", "$it")
-            val mediaController = MediaController(this)
+        //setContentView(binding.root)
+        var mostrar=findViewById<Button>(R.id.getImage)
 
 
+        mostrar.setOnClickListener {
+            /*val progressDialog=ProgressDialog(this)
+            progressDialog.setMessage("Cargando imagen...")
+            progressDialog.setCancelable(false)
+            progressDialog.show()*/
 
-            mediaController.setAnchorView(video)
 
-            video.setMediaController(mediaController)
+            //Leer el video de storageRef
+            var nombre_auto=intent.getStringExtra("TITULO AUTO").toString()
 
-            //video.setVideoPath(nombre_auto)
-            video.requestFocus()
-            video.start()
+            var imagen=findViewById<ImageView>(R.id.imageview)
+            val storageRef = storage.reference.child("autos/$nombre_auto")
+            //Log.d("Firebase", "$nombre_auto")
+
+            val localfile= File.createTempFile("tempImage","jpg")
+            storageRef.getFile(localfile).addOnSuccessListener {
+
+                /*if (progressDialog.isShowing){
+                    progressDialog.dismiss()
+                }*/
+
+                val bitmap=BitmapFactory.decodeFile(localfile.absolutePath)
+                imagen.setImageBitmap(bitmap)
+            }.addOnCanceledListener {
+                /*if (progressDialog.isShowing){
+                    progressDialog.dismiss()
+                }*/
+                Toast.makeText(this,"Fallo al cargar la imagen",Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+
         }
-
-
-
-
-
     }
 
 }
